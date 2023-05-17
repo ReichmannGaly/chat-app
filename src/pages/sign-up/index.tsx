eebbc95dcfced2023e272a15efeff6e626612873
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-05-17 14:35:31 +0300

    feat: add form verification when creating user

--- src/pages/sign-up/index.tsx
+++ src/pages/sign-up/index.tsx
@@ -1,11 +1,9 @@
-import React, { FormEventHandler, useState} from 'react';
+import React from 'react';
 import {useRouter} from "next/router";
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
@@ -13,6 +11,9 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import {Field, Form, Formik} from "formik";
+import {object, string} from "yup";
+import * as Yup from 'yup';
 
 function Copyright() {
     return (
@@ -26,52 +27,27 @@ function Copyright() {
 
 const theme = createTheme();
 
+const initialValues = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    name: "",
+    bio: ""
+}
+
 const SignUp = () => {
-    const [firstName, setFirstName] = useState<string>('');
-    const [lastName, setLastName ] = useState<string>('');
-    const [email, setEmail] = useState<string>('');
-    const [password, setPassword] = useState<string>('');
-    const [confirmPassword, setConfirmPassword] = useState<string>('');
-    const [bio, setBio] = useState<string>('');
-
-    const handleFirstNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setFirstName(event.target.value);
-    }
-
-    const handleLastNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setLastName(event.target.value);
-    }
-
-    const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEmail(event.target.value);
-    }
-
-    const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setPassword(event.target.value);
-    }
-
-    const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setConfirmPassword(event.target.value);
-    }
-
-    const handleBioChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setBio(event.target.value);
-    }
 
     const router = useRouter();
 
-    const handleSubmit: FormEventHandler<HTMLDivElement> = (e) => {
-        e.preventDefault();
-        if (password !== confirmPassword) {
-            alert('Passwords do not match. Please try again.');
-            return;
-        }
+    const handleSubmit = (values, { setSubmitting }) => {
 
         const user = {
-            email: email,
-            password: password,
-            name: `${firstName} ${lastName}`,
-            bio: bio
+            email: values.email,
+            password: values.password,
+            name: `${values.firstName} ${values.lastName}`,
+            bio: values.bio
         }
 
         fetch("http://localhost:8080/users", {
@@ -89,6 +65,8 @@ const SignUp = () => {
                 }
             })
             .catch(error => console.error(error));
+
+        setSubmitting(false);
     }
 
     return (
@@ -109,108 +87,133 @@ const SignUp = () => {
                     <Typography component="h1" variant="h5">
                         Sign up
                     </Typography>
-                    <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    autoComplete="given-name"
-                                    name="firstName"
-                                    required
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={
+                            object({
+                                firstName: string().required("Please enter your first name"),
+                                lastName: string().required("Please enter your last name"),
+                                email: string().required("Please enter your email").email("Invalid email"),
+                                password: string()
+                                    .required("Please enter your password")
+                                    .min(8, "Password should be minimum 8 characters long"),
+                                confirmPassword: string()
+                                    .required("Please confirm your password")
+                                    .oneOf([Yup.ref('password')], 'Passwords must match'),
+                                bio:  string().required("Please enter your bio")
+                            })}
+                        onSubmit={handleSubmit}
+                    >
+                        {({ errors, isValid, touched, dirty ,isSubmitting}) => (
+                            <Form noValidate sx={{ mt: 3 }}>
+                                <Grid container spacing={2}>
+                                    <Grid item xs={12} sm={6}>
+                                        <Field
+                                            as={TextField}
+                                            autoComplete="given-name"
+                                            variant="outlined"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="firstName"
+                                            name="firstName"
+                                            label="First Name"
+                                            autoFocus
+                                            error={Boolean(errors.firstName) && Boolean(touched.firstName)}
+                                            helperText={touched.firstName && errors.firstName}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm={6}>
+                                        <Field
+                                            as={TextField}
+                                            autoComplete="family-name"
+                                            variant="outlined"
+                                            margin="normal"
+                                            required
+                                            fullWidth
+                                            id="lastName"
+                                            name="lastName"
+                                            label="Last Name"
+                                            error={Boolean(errors.lastName) && Boolean(touched.lastName)}
+                                            helperText={touched.lastName && errors.lastName}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Field
+                                            as={TextField}
+                                            required
+                                            fullWidth
+                                            id="email"
+                                            label="Email Address"
+                                            name="email"
+                                            autoComplete="email"
+                                            error={Boolean(errors.email) && Boolean(touched.email)}
+                                            helperText={touched.email && errors.email}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Field
+                                            as={TextField}
+                                            required
+                                            fullWidth
+                                            name="password"
+                                            label="Password"
+                                            type="password"
+                                            id="password"
+                                            autoComplete="new-password"
+                                            error={Boolean(errors.password) && Boolean(touched.password)}
+                                            helperText={touched.password && errors.password}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Field
+                                            as={TextField}
+                                            required
+                                            fullWidth
+                                            name="confirmPassword"
+                                            label="Confirm password"
+                                            type="password"
+                                            id="confirmPassword"
+                                            autoComplete="new-password"
+                                            error={Boolean(errors.confirmPassword) && Boolean(touched.confirmPassword)}
+                                            helperText={touched.confirmPassword && errors.confirmPassword}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12}>
+                                        <Field
+                                            as={TextField}
+                                            required
+                                            fullWidth
+                                            variant="outlined"
+                                            multiline
+                                            name="bio"
+                                            label="Bio"
+                                            id="bio"
+                                            autoComplete="Bio"
+                                            error={Boolean(errors.bio) && Boolean(touched.bio)}
+                                            helperText={touched.bio && errors.bio}
+                                        />
+                                    </Grid>
+                                </Grid>
+                                <Button
+                                    type="submit"
                                     fullWidth
-                                    id="firstName"
-                                    label="First Name"
-                                    autoFocus
-                                    value={firstName}
-                                    onChange={handleFirstNameChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12} sm={6}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="lastName"
-                                    label="Last Name"
-                                    name="lastName"
-                                    autoComplete="family-name"
-                                    value={lastName}
-                                    onChange={handleLastNameChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    id="email"
-                                    label="Email Address"
-                                    name="email"
-                                    autoComplete="email"
-                                    value={email}
-                                    onChange={handleEmailChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="password"
-                                    label="Password"
-                                    type="password"
-                                    id="password"
-                                    autoComplete="new-password"
-                                    value={password}
-                                    onChange={handlePasswordChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    name="confirmPassword"
-                                    label="Confirm password"
-                                    type="password"
-                                    id="confirmPassword"
-                                    autoComplete="new-password"
-                                    value={confirmPassword}
-                                    onChange={handleConfirmPasswordChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <TextField
-                                    required
-                                    fullWidth
-                                    variant="outlined"
-                                    multiline
-                                    name="bio"
-                                    label="Bio"
-                                    id="bio"
-                                    autoComplete="Bio"
-                                    value={bio}
-                                    onChange={handleBioChange}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <FormControlLabel
-                                    control={<Checkbox value="allowExtraEmails" color="primary" />}
-                                    label="I agree to terms and conditions."
-                                />
-                            </Grid>
-                        </Grid>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Sign Up
-                        </Button>
-                        <Grid container justifyContent="flex-end">
-                            <Grid item>
-                                <Link href="/login" variant="body2">
-                                    Already have an account? Sign in
-                                </Link>
-                            </Grid>
-                        </Grid>
-                    </Box>
+                                    variant="contained"
+                                    sx={{ mt: 3, mb: 2 }}
+                                    disabled={!isValid || !dirty || isSubmitting}
+                                >
+                                    Sign Up
+                                </Button>
+                                <Grid container justifyContent="flex-end">
+                                    <Grid item>
+                                        <Link href="/login" variant="body2">
+                                            Already have an account? Sign in
+                                        </Link>
+                                    </Grid>
+                                </Grid>
+                            </Form>
+                        )}
+                    </Formik>
                 </Box>
                 <Copyright sx={{ mt: 5 }} />
             </Container>
