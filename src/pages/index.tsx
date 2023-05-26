29119f8f4445600fc4244c6137a06ea15e8dfbba
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-05-26 18:52:12 +0300

    fix: fix root page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,8 @@
 import styles from '@/styles/Home.module.css'
-import React, {useEffect} from "react";
-import {useRouter} from "next/router";
+import React from "react";
 import Typography from "@mui/material/Typography";
 
 export default function Home() {
-  const router = useRouter();
-
-  useEffect(() => {
-
-    if (sessionStorage.getItem("user")) {
-      router.push('/global-chat');
-    } else {
-      router.push('/login');
-    }
-  }, []);
   return (
       <div className={styles.container}>
         <Typography variant="h3" align="center">Chat App</Typography>
