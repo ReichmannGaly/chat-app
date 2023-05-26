ed682d230ba3efcf6e16469ef1704bd80b52c086
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-05-26 18:45:10 +0300

    fix: add user verification for each page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!sessionStorage.getItem('user')) {
+      router.push('/login');
+    }
+  }, [router]);
+
+  return <Component {...pageProps} />;
 }
