76b4e0dc663d61712af43014c7aebd0f677264cf
Jean Koletto Reichmann GALY <hei.reichmann.4@gmail.com>
2023-06-08 20:25:42 +0300

    feat: add users list in messages page

--- src/pages/messages/index.tsx
+++ src/pages/messages/index.tsx
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CssBaseline from "@mui/material/CssBaseline";
 import AppBarComponent from "@/components/AppBarComponent";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
+import {useRouter} from "next/router";
+import {ListItemAvatar} from "@mui/material";
+import Avatar from "@mui/material/Avatar";
+import Typography from "@mui/material/Typography";
 
 const drawerWidth = 240;
 
 const Messages = () => {
-    const messagesArray = ['Message1', "Message2", "Message3", "Message4", "Message5"];
+    const [users,setUsers] = useState([]);
+
+    const router = useRouter();
+
+    useEffect(() => {
+        fetch("http://localhost:8080/users", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("userToken"))}`
+            }
+        })
+            .then(response => {
+                if(response.status === 200){
+                    return response.json()
+                }
+                else {
+                    throw new Error(`Unexpected response status: ${response.status}`)
+                }
+            })
+            .then(data => {
+                setUsers(data.users);
+            })
+    },[])
 
     return (
         <Box sx={{ display: 'flex' }}>
@@ -24,13 +50,17 @@ const Messages = () => {
                 sx={{ flexGrow: 1, p: 3, width: { sm: `calc(100% - ${drawerWidth}px)` } }}
             >
                 <Toolbar />
-                {messagesArray.map((message, index) => (
+                {users.map((user, index) => (
                     <ListItem key={index} disablePadding>
-                        <ListItemButton>
-                            <ListItemIcon>
-                                {message}
-                            </ListItemIcon>
-                            <ListItemText/>
+                        <ListItemButton onClick={() => router.push(`/messages/${user.id}`)}>
+                            <ListItemAvatar>
+                                <Avatar>
+                                    {user.name.charAt(0).toUpperCase()}
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText>
+                                <Typography>{user.name}</Typography>
+                            </ListItemText>
                         </ListItemButton>
                     </ListItem>
                 ))}
